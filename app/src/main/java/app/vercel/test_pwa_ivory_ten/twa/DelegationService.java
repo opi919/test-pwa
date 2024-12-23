03ca23963b17297f8a4c8612f2d97ac938ae4639
opi919 <khandokermomen919@gmail.com>
2024-12-23 22:46:47 +0600

    aab generation successful

---
+++ app/src/main/java/app/vercel/test_pwa_ivory_ten/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.test_pwa_ivory_ten.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+

498587342073aa4fdc197876c2458d3e90fac775
opi919 <khandokermomen919@gmail.com>
2024-12-23 14:10:32 +0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
+
+const manifestForPlugIn ={
+  registerType: "prompt",
+  includeAssests: ["favicon.ico", "apple-touch-icon-180x180.png", "logo.svg"],
+  manifest: {
+    name: "Uplift",
+    short_name: "Uplift",
+    description: "A Fundraising Platform for Nonprofits",
+    icons: [
+      {
+        src: "icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+
+      {
+        src: "icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+}
+
+export default defineConfig({
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+})
